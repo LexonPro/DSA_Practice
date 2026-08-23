e1c906b9e5b9241e28a8d1887de57ce331c709dc
LexonPro <shikharkumarmaurya@gmail.com>
2026-08-23 16:31:46 +0530

    Fix recursive permutation bounds

--- Recursion/permutation.cpp
+++ Recursion/permutation.cpp
@@ -4,10 +4,11 @@ using namespace std;
 void permutation(string str,int beg,int end){
     if (beg == end)
     {
-        cout << str << endl;    
+        cout << str << endl;
+        return;    
     }
     
-    for (int i = 0; i < end; i++)
+    for (int i = beg; i <= end; i++)
     {
         swap(str[beg],str[i]);
         permutation(str,beg+1,end);
